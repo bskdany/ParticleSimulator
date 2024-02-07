fd9cd87349c8cd38216d74ae0a20568ca286d71f
bskdany <54674556+bskdany@users.noreply.github.com>
2024-02-06 22:22:05 -0500

    making everything ready for deployment

--- ParticleSimulation/src/main/java/module-info.java
+++ ParticleSimulation/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module org.example.particlesimulation {
     requires javafx.controls;
     requires javafx.fxml;
-    requires JavaFastPFOR;
-
 
     opens org.example.particlesimulation to javafx.fxml;
     exports org.example.particlesimulation;
---
+++ ParticleSimulation/src/main/java/org/example/particlesimulation/Main.java
@@ -0,0 +1,7 @@
+package org.example.particlesimulation;
+
+public class Main {
+    public static void main(String[] args) {
+        ParticleSimulationGUI.main(args);
+    }
+}
