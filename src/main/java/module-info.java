7655b6b1761dd9ad80dc106aa610b4b67df9e6d3
bskdany <54674556+bskdany@users.noreply.github.com>
2024-02-07 12:01:19 -0500

    reformatting stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
-module org.example.particlesimulation {
+module org.example.parclesimulator {
     requires javafx.controls;
     requires javafx.fxml;
-
     opens org.example.particlesimulation to javafx.fxml;
     exports org.example.particlesimulation;
 }
