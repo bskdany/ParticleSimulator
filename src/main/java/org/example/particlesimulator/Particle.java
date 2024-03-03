b125e22a3c82dc474f4b87407cb6692fd3099c9f
bskdany <54674556+bskdany@users.noreply.github.com>
2024-03-03 18:04:55 -0500

    remove mass and other dumb stuff in favour of local variables

--- src/main/java/org/example/particlesimulator/Particle.java
+++ src/main/java/org/example/particlesimulator/Particle.java
@@ -9,62 +9,24 @@ import java.util.stream.Stream;
 
 public class Particle {
     public double[] position;
-    public final double RADIUS;
-    public Color color;
-    public final double MASS;
-    public final int SPECIES;
-    private final double[] deltaPosition;
-    private double[] force;
     public double[] velocity;
-    public boolean mixedSpecies;
+    public double[] force;
+
+    public final double RADIUS;
+    public final Color color;
+    public final int SPECIES;
     public boolean isMoving;
-    public boolean isCluster;
-    public static int particleMissRate = 0;
-    public static int particleChecks = 0;
-    public int[] containingSpecies;
-    double[] directionVector = new double[2];
-    public double[] localAttractionFactor;
-    public int containingSpeciesCount;
+
     Particle(int x, int y, double radius, Color color, double mass, int species){
         this.RADIUS = radius;
-        this.MASS = mass;
         this.SPECIES = species;
         this.color = color;
         this.position = new double[]{x,y};
-        this.deltaPosition = new double[]{0, 0};
         this.force = new double[]{0,0};
         this.velocity = new double[]{0,0};
-        this.mixedSpecies = false;
-        localAttractionFactor = new double[7];
-        containingSpeciesCount = 1;
         isMoving = true;
     }
-    Particle(double[] position, int[] containingSpecies){
-        this.position = position;
-        this.mixedSpecies = true;
-        this.containingSpecies = containingSpecies;
-        this.deltaPosition = new double[]{0, 0};
-        this.force = new double[]{0,0};
-        this.velocity = new double[]{0,0};
-        RADIUS = 1;
-        MASS = 1;
-        SPECIES = 0;
-        // here I am calculating the cumulative attraction factor that the particle would have
-        // if any other particle want to calculate the force with it
-        // for example a yellow particle could consider this particle as attractive
-        // while a red one could consider it as repulsive
-        localAttractionFactor = new double[7];
 
-        for (int i = 0; i < 7; i++) {       // where 7 is the number of species
-            localAttractionFactor[i] = 0;
-
-            // if I was particle with species i (0) what is my attraction to this approximated particle?
-            for (int j = 0; j < 7; j++) {
-                localAttractionFactor[i] += AttractionMatrix.attractionMatrix[i][j] * containingSpecies[j];
-            }
-            containingSpeciesCount += containingSpecies[i];
-        }
-    }
     /**
      * Copy constructor for the particle
      * @param original the original particle
@@ -73,15 +35,8 @@ public class Particle {
         this.position = original.position.clone();
         this.RADIUS = original.RADIUS;
         this.color = original.color;
-        this.MASS = original.MASS;
         this.SPECIES = original.SPECIES;
-        this.deltaPosition = original.deltaPosition.clone();
-        this.force = original.force.clone();
         this.velocity = original.velocity.clone();
-        this.directionVector = new double[]{0,0};
-        this.mixedSpecies = original.mixedSpecies;
-        this.containingSpecies = original.containingSpecies;
-        this.localAttractionFactor = original.localAttractionFactor;
     }
 
     public void adjustPositionWrapping(){
@@ -134,103 +89,20 @@ public class Particle {
 
             OptimizationTracking.getInstance().increaseUsedInCalculation();
 
+            double attractionFactor = AttractionMatrix.attractionMatrix[SPECIES][targetParticle.SPECIES];
 
-            double attractionFactor;
-            if(targetParticle.mixedSpecies){
-                attractionFactor = targetParticle.localAttractionFactor[SPECIES];
-            }
-            else {
-                attractionFactor = AttractionMatrix.attractionMatrix[SPECIES][targetParticle.SPECIES];
-            }
-
-            double magnitude = calculateAttractionForce(distance, attractionFactor)* targetParticle.containingSpeciesCount;
+            double magnitude = calculateAttractionForce(distance, attractionFactor);
             double[] normalisedDirectionVector = normalizeVector(directionVector);
 
             force[0] += normalisedDirectionVector[0] * magnitude * ParticleSimulation.maxAttractionDistance;
             force[1] += normalisedDirectionVector[1] * magnitude * ParticleSimulation.maxAttractionDistance;
         });
-    }
-
-
-    public double[] calculateCumulativeParticleForce(){
-        double[] force = new double[2];
-        OptimizationTracking tracking = OptimizationTracking.getInstance();
-
-        ParticleSimulation.particleGridMap.getParticleAround(this).forEach(targetParticle -> {
-            tracking.increaseTotalInteractions();
-
-            if(!targetParticle.isMoving && !isMoving){
-                tracking.increaseImmobileCounter();
-                return;
-            }
-
-            double[] directionVector = getParticleDirectionVector(this, targetParticle);
-
-            if(Math.abs(directionVector[0]) > ParticleSimulation.maxAttractionDistance && Math.abs(directionVector[0]) < ParticleSimulation.wrapDirectionLimitWidth){
-                tracking.increaseDiscardedOutOfRange();
-                return;
-
-            }
-            if(Math.abs(directionVector[1]) > ParticleSimulation.maxAttractionDistance && Math.abs(directionVector[1]) < ParticleSimulation.wrapDirectionLimitHeight){
-                tracking.increaseDiscardedOutOfRange();
-                return;
-            }
-
-            Particle.calculateVectorWrap(directionVector);
-
-            // length of the relative distance
-            double distance = Math.sqrt(directionVector[0] * directionVector[0] + directionVector[1] * directionVector[1]) / ParticleSimulation.maxAttractionDistance;
-
-            if(distance > 1) {
-                OptimizationTracking.getInstance().increaseDiscardedOutOfRange();
-                return;
-            }
-
-            OptimizationTracking.getInstance().increaseUsedInCalculation();
-
-
-            double attractionFactor;
-            if(targetParticle.mixedSpecies){
-                attractionFactor = targetParticle.localAttractionFactor[SPECIES];
-            }
-            else {
-                attractionFactor = AttractionMatrix.attractionMatrix[SPECIES][targetParticle.SPECIES];
-            }
-
-            double magnitude = calculateAttractionForce(distance, attractionFactor)* targetParticle.containingSpeciesCount;
-            double[] normalisedDirectionVector = normalizeVector(directionVector);
-
-            force[0] += normalisedDirectionVector[0] * magnitude * ParticleSimulation.maxAttractionDistance;
-            force[1] += normalisedDirectionVector[1] * magnitude * ParticleSimulation.maxAttractionDistance;
-        });
-        return force;
     }
 
     public void simulate(){
-//        force[0] = 0;
-//        force[1] = 0;
-
-//        ParticleForceCache particleForceCache = ParticleForceCache.getInstance();
-//        double[] tempForce = particleForceCache.getCachedForce(SPECIES, position);
-//        if(tempForce != null){
-//            force = tempForce;
-//            OptimizationTracking.getInstance().increaseCachingCounter();
-//        }
-//        else{
-//            force = calculateCumulativeParticleForce();
-//            particleForceCache.cacheForce(SPECIES,position, force);
-//        }
-
-//        force = calculateCumulativeParticleForce();
-
-        // all particles move towards the center slowly
-//        double[] vectorTowardsCenter = normalizeVector(new double[] {(( ParticleSimulation.CANVAS_WIDTH / 2) - position[0]), ( ParticleSimulation.CANVAS_HEIGHT / 2) - position[1]});
-//        force[0] += vectorTowardsCenter[0] * ParticleSimulation.CENTRAL_ATTRACTION_MULTIPLIER;
-//        force[1] += vectorTowardsCenter[1] * ParticleSimulation.CENTRAL_ATTRACTION_MULTIPLIER;
-
         // F = m / a
-        double accelerationX = force[0] * ParticleSimulation.forceMultiplier / MASS;
-        double accelerationY = force[1] * ParticleSimulation.forceMultiplier / MASS;
+        double accelerationX = force[0] * ParticleSimulation.forceMultiplier;
+        double accelerationY = force[1] * ParticleSimulation.forceMultiplier;
 
         velocity[0] *= ParticleSimulation.friction;
         velocity[1] *= ParticleSimulation.friction;
@@ -241,7 +113,7 @@ public class Particle {
                             // I made this value up
         isMoving = Math.pow(velocity[0] * velocity[1], 2) > 1;
 
-
+        double[] deltaPosition = new double[]{0,0};
         deltaPosition[0] = velocity[0] * ParticleSimulation.UPDATE_RATE_MS / 1000;
         deltaPosition[1] = velocity[1] * ParticleSimulation.UPDATE_RATE_MS / 1000;
 
@@ -270,7 +142,7 @@ public class Particle {
         return 0;
     }
 
-    private static double[] calculateVectorWrap(double[] directionVector){
+    private static void calculateVectorWrap(double[] directionVector){
         // vector from source to target
         if(directionVector[0] > ParticleSimulation.wrapDirectionLimitWidth){  // warp left
             directionVector[0] = directionVector[0] - ParticleSimulation.CANVAS_WIDTH -1;
@@ -285,7 +157,6 @@ public class Particle {
         else if(directionVector[1] < -ParticleSimulation.wrapDirectionLimitHeight){ // warp bottom
             directionVector[1] = directionVector[1] + ParticleSimulation.CANVAS_HEIGHT +1;
         }
-        return directionVector;
     }
 
     public static List<Particle> deepCloneList(List<Particle> originalList){
