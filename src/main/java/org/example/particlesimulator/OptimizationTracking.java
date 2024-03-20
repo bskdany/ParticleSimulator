20e7800a07f0afbcb5078d3b2d82c9ddc40292ce
bskdany <54674556+bskdany@users.noreply.github.com>
2024-03-19 23:22:43 -0400

    making optimization tracking variables atomic integers for 100% accurate stuff

--- src/main/java/org/example/particlesimulator/OptimizationTracking.java
+++ src/main/java/org/example/particlesimulator/OptimizationTracking.java
@@ -1,53 +1,44 @@
 package org.example.particlesimulator;
 
 import java.text.DecimalFormat;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OptimizationTracking {
     private static OptimizationTracking instance;
-    private int calculationSavedByCaching;
-    private int immobileParticles;
-    private int rogueParticles;
-    private int discardedDueToOutOfRange;
-    private int usedInCalculation;
-    private int totalParticleInteractions;
-    private int numberOfParticlesAveraged;
-    private int randomRejectedParticles;
+    private final AtomicInteger immobileParticles = new AtomicInteger();
+    private final AtomicInteger discardedDueToOutOfRange = new AtomicInteger();
+    private final AtomicInteger usedInCalculation = new AtomicInteger();
+    private final AtomicInteger totalParticleInteractions = new AtomicInteger();
+    private final AtomicInteger numberOfParticlesAveraged = new AtomicInteger();
+    private final AtomicInteger randomRejectedParticles = new AtomicInteger();
     private int numberOfParticles;
     private int updateCounter;
     private int totalParticleCount;
-    private int collisionCount;
     OptimizationTracking(){
         resetValues();
     }
 
-    public void increaseCachingCounter(){
-        calculationSavedByCaching++;
-    }
     public void increaseImmobile(int num){
-        immobileParticles+=num;
+        immobileParticles.addAndGet(num);
     }
-    public void increaseRogueParticles(){rogueParticles++;}
-    public void setImmobile(int num){immobileParticles+=num;}
-    public void setRogue(int num){rogueParticles+=num;}
-    public void increaseCollision(){collisionCount++;}
-
+    public void setImmobile(int num){immobileParticles.addAndGet(num);}
     public void increaseDiscardedOutOfRange(int num){
-        discardedDueToOutOfRange+=num;
+        discardedDueToOutOfRange.addAndGet(num);
     }
     public void increaseUsedInCalculation(int num){
-        usedInCalculation+=num;
+        usedInCalculation.addAndGet(num);
     }
-    public void increaseParticlesAveraged(){numberOfParticlesAveraged++;}
-    public void increaseTotalInteractions(int num){totalParticleInteractions+=num;}
-    public void increaseRandomRejected(int num){randomRejectedParticles+=num;}
-    public void increaseUpdate(){updateCounter++;}
+    public void increaseParticlesAveraged(){numberOfParticlesAveraged.addAndGet(1);}
+    public void increaseTotalInteractions(int num){totalParticleInteractions.addAndGet(num);}
+    public void increaseRandomRejected(int num){randomRejectedParticles.addAndGet(num);}
+    public void increaseUpdate(){updateCounter+=1;}
     public void setParticleCount(int num){totalParticleCount = num;}
     public void showOptimizationData(){
-        System.out.println("Averaged    " + calculatePercentage(numberOfParticlesAveraged, numberOfParticles * updateCounter));
-        System.out.println("Immobile    " + calculatePercentage(immobileParticles, totalParticleInteractions));
-        System.out.println("Rejected    " + calculatePercentage(randomRejectedParticles, totalParticleInteractions));
-        System.out.println("Range       " + calculatePercentage(discardedDueToOutOfRange, totalParticleInteractions));
-        System.out.println("Used        " + calculatePercentage(usedInCalculation, totalParticleInteractions));
+        System.out.println("Averaged    " + calculatePercentage(numberOfParticlesAveraged.intValue(), numberOfParticles * updateCounter));
+        System.out.println("Immobile    " + calculatePercentage(immobileParticles.intValue(), totalParticleInteractions.intValue()));
+        System.out.println("Rejected    " + calculatePercentage(randomRejectedParticles.intValue(), totalParticleInteractions.intValue()));
+        System.out.println("Range       " + calculatePercentage(discardedDueToOutOfRange.intValue(), totalParticleInteractions.intValue()));
+        System.out.println("Used        " + calculatePercentage(usedInCalculation.intValue(), totalParticleInteractions.intValue()));
         System.out.println();
 
         resetValues();
@@ -59,17 +50,14 @@ public class OptimizationTracking {
     }
 
     private void resetValues(){
-        calculationSavedByCaching = 0;
-        rogueParticles = 0;
-        immobileParticles = 0;
-        discardedDueToOutOfRange = 0;
-        usedInCalculation = 0;
-        totalParticleInteractions = 1;
-        numberOfParticlesAveraged = 0;
+        immobileParticles.set(0);
+        discardedDueToOutOfRange.set(0);
+        usedInCalculation.set(0);
+        totalParticleInteractions.set(0);
+        numberOfParticlesAveraged.set(0);
         numberOfParticles = 1500 * 7;
         updateCounter = 0;
-        randomRejectedParticles = 0;
-        collisionCount = 0;
+        randomRejectedParticles.set(0);
     }
 
     public static OptimizationTracking getInstance() {
