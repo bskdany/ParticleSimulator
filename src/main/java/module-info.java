02ee986a98d9b78d579ae004130260ca474a8610
bskdany <54674556+bskdany@users.noreply.github.com>
2024-03-20 11:14:11 -0400

    fixed controller default values

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.parclesimulator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires aparapi;
     opens org.example.particlesimulator to javafx.fxml;
     exports org.example.particlesimulator;
 }
